bc985a6c9a0162ae34b133bb0ce296efacf31632
Aung Aung Htet <38067432+aungaunghtet29@users.noreply.github.com>
2020-02-07 20:52:41 +0630

    Create GoByDrivingAlgo.java

---
+++ Strategy Pattern/GoByDrivingAlgo.java
@@ -0,0 +1,9 @@
+package demo.strategy.pattern;
+
+public class GoByDrivingAlgo implements GoAlgorithms{
+	@Override
+	public String go() {
+		// TODO Auto-generated method stub
+		return "I am driving now";
+	}
+}
