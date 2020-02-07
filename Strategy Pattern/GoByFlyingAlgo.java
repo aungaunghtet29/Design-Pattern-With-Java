7b299b1b21a8e3d8badfadbada0a0dd9af896cfd
Aung Aung Htet <38067432+aungaunghtet29@users.noreply.github.com>
2020-02-07 20:53:02 +0630

    Create GoByFlyingAlgo.java

---
+++ Strategy Pattern/GoByFlyingAlgo.java
@@ -0,0 +1,9 @@
+package demo.strategy.pattern;
+
+public class GoByFlyingAlgo implements GoAlgorithms{
+	@Override
+	public String go() {
+		// TODO Auto-generated method stub
+		return "I'm Flying Now";
+	}
+}
